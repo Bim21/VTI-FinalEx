4663173c1e08e23bd8996f19ce7a992df145f299
Bim21 <chiennguyen2198@gmail.com>
2023-11-06 09:41:35 +0700

    first commit

---
+++ FinalEx/src/main/java/com/vti/entity/Account.java
@@ -0,0 +1,13 @@
+package com.vti.entity;
+
+
+public class Account {
+    private int id;
+    private String userName;
+    private String passWord;
+    private String firstName;
+    private String lastName;
+    private Role role;
+    private Department departmentId;
+
+}
---
+++ FinalEx/src/main/java/com/vti/entity/Department.java
@@ -0,0 +1,12 @@
+package com.vti.entity;
+
+import java.util.Date;
+
+public class Department {
+    private int id;
+    private String departmentName;
+    private int totalMember;
+    private Type type;
+
+    private Date createDate;
+}
